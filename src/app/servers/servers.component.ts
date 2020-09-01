8bf3642326549ecf46def579f6e9f32f87e073aa
hdamjan <damjan.hudobreznik@inova.si>
2020-09-01 10:10:09 +0200

    Implement two-way binding for input field.

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -23,8 +23,4 @@ export class ServersComponent implements OnInit {
     this.serverCreationStatus = 'Server was created';
   }
 
-  onUpdateServerName(event: Event) {
-    this.serverName = (<HTMLInputElement>event.target).value;
-  }
-
 }
